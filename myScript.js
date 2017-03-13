912b86e748e83203f5d035f18ab81ce477783b1d
Raimonds Kramēns <x161REB132@sc.etf.rtu.lv>
2017-03-13 10:38:08 +0200

    First commit

---
+++ myScript.js
@@ -0,0 +1,12 @@
+function bulbOn()
+{
+document.getElementById('myImage').src='pic_bulbon.gif';
+document.getElementById('demo').style.fontSize='35px';
+}
+
+
+function bulbOff()
+{
+document.getElementById('myImage').src='pic_bulboff.gif';
+document.getElementById('demo').style.fontSize='16px';
+}
